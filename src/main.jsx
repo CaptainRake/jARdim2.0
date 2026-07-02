9794aeb80a2b35dfb58356efcf837cc645a8225a
CaptainRake <juliacarmellicampos@gmail.com>
2026-07-02 09:41:12 -0300

    corrige basename github pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/jARdim2.0">
       <App />
     </BrowserRouter>
   </React.StrictMode>
